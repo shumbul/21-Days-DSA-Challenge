7e8c86683347b2c0a8ff163dc2b82ba721658e90
Shumbul Arifa <arifas18922@gmail.com>
2023-12-20 18:21:52 +0530

    Update Count pairs with given sum.cpp

--- Count pairs with given sum.cpp
+++ Count pairs with given sum.cpp
@@ -4,13 +4,22 @@ class Solution{
 public:
     int getPairsCount(int arr[], int n, int k) {
         // code here
-        
+        int ans=0;
+        unordered_map<int, int> mp;
+        for(int i=0;i<n;i++){
+            if(mp.find(k-arr[i])!=mp.end()){
+                ans+=mp[k-arr[i]];
+            }
+            mp[arr[i]]++;
+        }
+        return ans;
     }
+};
 };
 
 /*
-Time complexity: O()
-Space complexity: O()
+Time complexity: O(n)
+Space complexity: O(n)
 */
 
 
