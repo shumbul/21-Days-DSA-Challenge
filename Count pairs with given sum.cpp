12bf53ae2b83b4826253a38cfeae09668c70d079
Shumbul Arifa <arifas18922@gmail.com>
2023-12-20 17:09:28 +0530

    Create Count pairs with given sum.cpp

---
+++ Count pairs with given sum.cpp
@@ -0,0 +1,17 @@
+// https://bit.ly/47bCCoF
+
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        // code here
+        
+    }
+};
+
+/*
+Time complexity: O()
+Space complexity: O()
+*/
+
+
+// Code by Shumbul Arifa - https://linktr.ee/shumbul 
