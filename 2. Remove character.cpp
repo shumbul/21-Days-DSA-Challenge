ea0c2ee759ccbc92cfbc7e24fa86c618304ed0b5
Kodi Prem Sai <108254753+PremsaiKodi@users.noreply.github.com>
2023-12-21 17:05:21 +0530

    Update 2. Remove character.cpp

--- 2. Remove character.cpp
+++ 2. Remove character.cpp
@@ -4,7 +4,15 @@ class Solution {
   public:
     string removeChars(string string1, string string2) {
         // code here
-
+        map<char,int>mp;
+        for(auto it:string2) mp[it]++;
+        for(int i=0; i<string1.size(); i++){
+            if(mp.find(string1[i]) != mp.end()){
+                string1.erase(string1.begin() + i);
+                i--;
+            }
+        }
+        return string1;
     }
 };
 
