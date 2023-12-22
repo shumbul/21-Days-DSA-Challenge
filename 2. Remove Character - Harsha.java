230182e47586627420e821052dc2f244ebe0766d
harsha_bulusu <sriharsha16208@gmail.com>
2023-12-22 06:40:36 +0530

    Create 2. Remove Character - Harsha.java

---
+++ 2. Remove Character - Harsha.java
@@ -0,0 +1,22 @@
+class Solution{
+    static String removeChars(String string1, String string2){
+        // code here
+       Set<Character> set = new HashSet<>();
+       
+       for (int i = 0; i < string2.length(); i++) {
+           set.add(string2.charAt(i));
+       }
+       
+       String res = "";
+       
+       for (int i = 0; i < string1.length(); i++) {
+           char ch = string1.charAt(i);
+           if (set.add(ch)) {
+                res += ch;   
+                set.remove(ch);
+           } 
+       }
+       
+       return res;
+    }
+}
