a19bc57d2a4fd31a35d5d5f2c5bf2942af2c56c2
harsha_bulusu <sriharsha16208@gmail.com>
2023-12-22 11:22:01 +0530

    Create CountPairsWithGivenSum.java

---
+++ CountPairsWithGivenSum.java
@@ -0,0 +1,25 @@
+class Solution {
+ 
+ 
+ int getPairsCount(int[] arr, int n, int k) {
+ // code here
+ Map<Integer, Integer> map = new HashMap<>();
+ int count = 0;
+ for (int i : arr) {
+ if (map.containsKey(k - i)) {
+ count += map.get(k - i);
+ }
+ 
+ if (map.containsKey(i)) {
+ map.put(i, map.get(i) + 1);
+ } else {
+ map.put(i, 1);
+ }
+ }
+ return count;
+ }
+}
+
+/** Time complexity : O(n) - for iterating array of n elements
+ * Space complexity: O(n) - For storing n elements of array (in worst case where all elements of the array are unique)
+*/
