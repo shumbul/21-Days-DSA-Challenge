8146511a0b195c0b531bdbb3f01d7dd6b97dc2dd
Shumbul Arifa <arifas18922@gmail.com>
2023-12-23 18:17:25 +0530

    Update 4. Mirror Tree.cpp

--- 4. Mirror Tree.cpp
+++ 4. Mirror Tree.cpp
@@ -1,1 +1,22 @@
+// Problem - https://bit.ly/4aqvKa0
+// Video solution on my YouTube - https://yt.openinapp.co/shumbul
 
+class Solution {
+    public:
+      // Function to convert a binary tree into its mirror tree.
+      void mirror(Node* node) {
+          // code here
+          if(!node)
+              return;
+          swap(node->left, node->right);
+          mirror(node->left);
+          mirror(node->right);
+      }
+};
+
+/*
+    Time complexity: O(n), n=number of nodes
+    Space complexity: O(h), h=height of tree
+*/
+
+// Code by Shumbul Arifa - https://linktr.ee/shumbul 
