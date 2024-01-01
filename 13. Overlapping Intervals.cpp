96ccf508df51b7daee5ea48d4b4bc6ad8dff5df0
Shumbul Arifa <arifas18922@gmail.com>
2024-01-01 19:55:03 +0530

    Update 13. Overlapping Intervals.cpp

--- 13. Overlapping Intervals.cpp
+++ 13. Overlapping Intervals.cpp
@@ -1,1 +1,31 @@
+// Problem link - https://bit.ly/48zi1vM
 
+class Solution {
+public:
+    vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) {
+         // Code here
+        sort(intervals.begin(), intervals.end());
+        int interval=0, n=intervals.size();
+        vector<vector<int>> ans;
+        while(interval<n){
+            int start=intervals[interval][0];
+            int end=intervals[interval][1];
+            interval++;
+            while(interval<n && intervals[interval][0]<=end){
+                end=max(end, intervals[interval][1]);
+                interval++;
+            }
+            ans.push_back({start, end});
+        }
+        return ans;
+    }
+};
+
+/*
+    Time complexity: O(nlogn) - sorting
+    Space complexity: O(n)
+*/
+
+// Code by Shumbul Arifa - https://linktr.ee/shumbul 
+// Follow 21 days DSA Challenge - www.shumbularifa.com
+// Video solutions available on my YouTube
